7099937783928652a73771950551d8a7751879bc
Deep Awasthi <da.madskull@gmail.com>
2024-04-02 15:05:16 +0530

    Update StatusRepository.java

--- src/main/java/com/deepawasthi/friendsbook/Repositories/StatusRepository.java
+++ src/main/java/com/deepawasthi/friendsbook/Repositories/StatusRepository.java
@@ -2,9 +2,11 @@ package com.deepawasthi.friendsbook.Repositories;
 
 import com.deepawasthi.friendsbook.Entities.StatusEntity;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface StatusRepository extends CassandraRepository<StatusEntity, UUID> {
 
 }
