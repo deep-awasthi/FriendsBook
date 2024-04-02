adbd62da2646a69cdee327bac1279b44d09a3d91
Deep Awasthi <da.madskull@gmail.com>
2024-04-02 15:04:32 +0530

    Update CommentRepository.java

--- src/main/java/com/deepawasthi/friendsbook/Repositories/CommentRepository.java
+++ src/main/java/com/deepawasthi/friendsbook/Repositories/CommentRepository.java
@@ -3,10 +3,12 @@ package com.deepawasthi.friendsbook.Repositories;
 import com.deepawasthi.friendsbook.Entities.CommentEntity;
 import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface CommentRepository extends CassandraRepository<CommentEntity, UUID> {
 
     @AllowFiltering
