19cf59e16b88402b91b1129e089dae44c34d4bb1
Deep Awasthi <da.madskull@gmail.com>
2024-04-02 15:05:43 +0530

    Update UserRepository.java

--- src/main/java/com/deepawasthi/friendsbook/Repositories/UserRepository.java
+++ src/main/java/com/deepawasthi/friendsbook/Repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.deepawasthi.friendsbook.Repositories;
 
 import com.deepawasthi.friendsbook.Entities.UserEntity;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends CassandraRepository<UserEntity, UUID> {
 
 }
